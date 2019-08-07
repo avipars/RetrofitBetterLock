b02f530560198174f49674b1711ec8c5c1d062e3
avipars <avi.pars@gmail.com>
2019-08-07 21:13:23 +0300

    added context menu. Got rest of actions working. TODO: better design for grid and get add to home screen working

--- app/src/main/java/com/aviparshan/betterlock/Main.java
+++ app/src/main/java/com/aviparshan/betterlock/Main.java
@@ -1,16 +1,26 @@
 package com.aviparshan.betterlock;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -18,6 +28,7 @@ import com.aviparshan.betterlock.datamodel.AppDataModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,13 +42,27 @@ public class Main extends AppCompatActivity implements RecyclerAdapter.onItemCli
     private List<AppDataModel> appDataModels = new ArrayList<AppDataModel>();
     private LinearLayoutManager linearLayoutManager;
     private GridLayoutManager gridLayoutManager;
+    private static Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity_tool);
+
+        AppBarLayout mAppBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        final Toolbar tool = findViewById(R.id.toolbar);
         loadProgress = (ProgressBar) findViewById(R.id.progressBar);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        setSupportActionBar(tool);
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        tool.setTitleTextColor(Color.parseColor("#FFFFFF")); //works perfectly
+
+        mContext = this;
+
 
 //        int mNoOfColumns = 1;
 //        gridLayoutManager = new GridLayoutManager(Main.this, mNoOfColumns);
@@ -73,6 +98,8 @@ public class Main extends AppCompatActivity implements RecyclerAdapter.onItemCli
                 Log.d("Response", t.getMessage());
             }
         });
+
+
     }
 
     void openActivity(int number) {
@@ -111,49 +138,123 @@ public class Main extends AppCompatActivity implements RecyclerAdapter.onItemCli
 
     @Override
     public void itemDetailLongClick(AppDataModel conversion) {
-        launchActivity(conversion);
+//        uninstallApp(conversion);
     }
 
-//    TODO: fix edgelighting
+
+    /**
+     * <p>Intent to show an applications details page in (Settings) com.android.settings</p>
+     *
+     * @param model   The package name of the application
+     * @return the intent to open the application info screen.
+     */
+    public static void appInfo(AppDataModel model) {
+        String packageName = model.getPackageName();
+        Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + packageName));
+        mContext.startActivity(intent);
+    }
+
+    /**
+     * Run when user clicks uninstall app if he has a version from put of play store or is using an emulator
+     */
+    public static void deleteApp(AppDataModel model) {
+        String packageName = model.getPackageName();
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        intent.setData(Uri.parse("package:" + packageName));
+        mContext.startActivity(intent);
+    }
+
+    /**
+     * Run when user clicks uninstall app if he has a version from put of play store or is using an emulator
+     */
+    public static void uninstallApp(AppDataModel model) {
+        String packageName = model.getPackageName();
+        Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+        intent.setData(Uri.parse("package:" + packageName));
+        mContext.startActivity(intent);
+    }
+
+
+    /**
+     * <p>Intent to show an applications details page in (Settings) com.android.settings</p>
+     *
+     * @param packageName   The package name of the application
+     * @return the intent to open the application info screen.
+     */
+    public static void appInfo(String packageName) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + packageName));
+        mContext.startActivity(intent);
+    }
+
+    /**
+    * Run when user clicks uninstall app if he has a version from put of play store or is using an emulator
+    */
+     public static void deleteApp(String packageName) {
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        intent.setData(Uri.parse("package:" + packageName));
+         mContext.startActivity(intent);
+    }
+
     void launchActivity(AppDataModel app) {
-        if (!app.getActivity().isEmpty()) {
-        Toast.makeText(this, app.getActivity() + app.getTitle(), Toast.LENGTH_SHORT).show();
+        if (!app.getActivity().isEmpty() && !app.getPackageName().isEmpty()) {
             try {
                 Intent intent = new Intent();
                 intent.setComponent(new ComponentName(app.getPackageName(), app.getActivity()));
                 startActivity(intent);
             } catch (Exception e) {
-
                 Toast.makeText(this, e.toString() + " " + app.getPackageName(), Toast.LENGTH_SHORT).show();
                 Log.e("Error Loading", "Error: " + e.toString() + app.getPackageName());
                 app.setVersion("Not installed");
                 recyclerAdapter.notifyDataSetChanged();
-
-//                Intent intent = new Intent();
-//                intent.setComponent(new ComponentName("com.samsung.android.edgelightingeffectunit", "com.samsung.android.edgelightingeffectunit.activity.EdgeLightingUnitActivity"));
-//                startActivity(intent);
-
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage(app.getPackageName());
-                startActivity(launchIntent);
-
-            }
-        }
-        else {
-            Toast.makeText(this, app.getActivity() + app.getTitle(), Toast.LENGTH_SHORT).show();
-
-            try {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage(app.getPackageName());
-                Intent launchIntent2 = getPackageManager().getLaunchIntentForPackage("com.samsung.android.app.cocktailbarservice");
-
-                startActivity(launchIntent);
-            } catch(Exception e)
-            {
-                Toast.makeText(this, e.toString() + " " + app.getPackageName(), Toast.LENGTH_SHORT).show();
-                Log.e("Error Loading, Empty", "Error: " + e.toString() + app.getPackageName());
-
-                app.setVersion("Not installed");
-                recyclerAdapter.notifyDataSetChanged();
+//                Intent launchIntent = getPackageManager().getLaunchIntentForPackage(app.getPackageName());
+//                startActivity(launchIntent);
             }
         }
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.info:
+                appInfo(this.getPackageName());
+                return true;
+            case R.id.unin:
+                deleteApp(this.getPackageName());
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
--- app/src/main/java/com/aviparshan/betterlock/RecyclerAdapter.java
+++ app/src/main/java/com/aviparshan/betterlock/RecyclerAdapter.java
@@ -2,18 +2,23 @@ package com.aviparshan.betterlock;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.view.menu.MenuPopupHelper;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
-import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.aviparshan.betterlock.datamodel.AppDataModel;
 import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
@@ -59,14 +64,10 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         appDataModel = appDataModels.get(i);
         viewHolder.tv_actorname.setText(appDataModel.getTitle());
         viewHolder.tv_actorcountry.setText(appDataModel.getDescription());
-
         viewHolder.tv_actorgender.setText(appDataModel.getVersion());
-
         viewHolder.tv_actordateofbirth.setText(appDataModel.getPackageName());
-
         String url = appDataModel.getIcon();
 
-        final int position = i;
 
         Picasso.with(context)
                 .load(url)
@@ -79,6 +80,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
         viewHolder.setIsRecyclable(false);
 
+        final int position = i;
+
         viewHolder.main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -89,10 +92,46 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         viewHolder.main.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                listener.itemDetailClick(appDataModels.get(position));
-                return true;
+                listenerLong.itemDetailLongClick(appDataModels.get(position));
+                return pop(v, position);
             }
         });
+    }
+
+    boolean pop(View v, final int position)
+    {
+        PopupMenu popup = new PopupMenu(v.getContext(), v);
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.unin:
+                        Main.uninstallApp(appDataModels.get(position));
+                        return true;
+                    case R.id.info:
+                        Main.appInfo(appDataModels.get(position));
+                        return true;
+                    case R.id.home:
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        // here you can inflate your menu
+        popup.inflate(R.menu.context_menu);
+        popup.setGravity(Gravity.START);
+
+        // if you want icon with menu items then write this try-catch block.
+        try {
+            Field mFieldPopup=popup.getClass().getDeclaredField("mPopup");
+            mFieldPopup.setAccessible(true);
+            MenuPopupHelper mPopup = (MenuPopupHelper) mFieldPopup.get(popup);
+        } catch (Exception e) {
+            Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+        popup.show();
+        return true;
     }
 
     @Override
@@ -100,7 +139,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         return appDataModels.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private AppCompatImageView img_actor;
         private AppCompatTextView tv_actorname, tv_actorcountry, tv_actordateofbirth, tv_actorgender;
         private LinearLayout main;
@@ -115,16 +155,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
             main = itemView.findViewById(R.id.main);
 
-            itemView.setOnCreateContextMenuListener(this);
 
         }
 
-        @Override
-        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-            menu.setHeaderTitle("Select The Action");
-            menu.add(0, v.getId(), 0, "Call");//groupId, itemId, order, title
-            menu.add(0, v.getId(), 0, "SMS");
 
-        }
     }
 }
